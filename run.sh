798f5771bd891cd7fd30c50546d93b13303a9ce1
Your Name <you@example.com>
2020-12-25 19:23:14 +0530

    test4

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-echo $(git diff-tree --no-commit-id --name-only -r ${{ github.sha }})
-for i in ./div1/*.json; do curl -X POST 'http://164.90.148.125/indexes/movies/documents' --data @$i; done
+for i in $(git diff --name-only HEAD HEAD~1 | awk '/^div1\/.*json/{print $0}'); do echo "Updating $i"; curl -X POST 'http://164.90.148.125/indexes/movies/documents' --data @$i; done
+#for i in ./div1/*.json; do curl -X POST 'http://164.90.148.125/indexes/movies/documents' --data @$i; done
