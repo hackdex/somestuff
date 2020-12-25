09602588531cc3b8df4dcdb8dcf231dfa40fb13a
Your Name <you@example.com>
2020-12-25 19:39:57 +0530

    test7

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+echo $CHANGED_FILES;
 for i in $(echo $CHANGED_FILES | awk '/^div1\/.*json/{print $0}'); do echo "Updating $i"; curl -X POST 'http://164.90.148.125/indexes/movies/documents' --data @$i; done
 #for i in ./div1/*.json; do curl -X POST 'http://164.90.148.125/indexes/movies/documents' --data @$i; done
