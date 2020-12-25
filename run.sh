bc8d4931378d2d8567c4f08dbece0b837bfc9457
Your Name <you@example.com>
2020-12-25 19:15:06 +0530

    test3

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+echo $(git diff-tree --no-commit-id --name-only -r ${{ github.sha }})
 for i in ./div1/*.json; do curl -X POST 'http://164.90.148.125/indexes/movies/documents' --data @$i; done
